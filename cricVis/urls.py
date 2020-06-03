a47b31bee61374d05784b744cf684156f01efacd
IshaGupta18 <ishagupta1828@gmail.com>
2020-06-03 10:43:37 +0530

    chore: add URL for Google login

--- cricVis/urls.py
+++ cricVis/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from django.urls import path, include
+from django.conf import settings
 from . import views
 app_name = 'cricVis'
 urlpatterns = [
     url(r'^$', views.index, name='index'), #URL for home page
     url(r'^fetchGraphData/$', views.fetchGraphData, name='fetchGraphData'), #URL for GET function to fetch chart response
+    path('', include('social_django.urls', namespace='social')),
 ]
