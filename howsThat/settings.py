8a8cd4eda9607943c9fbffbff436705d40228e42
IshaGupta18 <ishagupta1828@gmail.com>
2020-06-03 10:40:32 +0530

    chore: add redirect paths and client ID

--- howsThat/settings.py
+++ howsThat/settings.py
@@ -107,6 +107,15 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
+SOCIAL_AUTH_URL_NAMESPACE = 'social'
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ.get('CLIENT_ID_CRICVIS')
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.environ.get('CLIENT_SECRET_CRICVIS')
+
+LOGIN_URL = '/auth/login/google-oauth2/'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
