f17a2a61441c5615da001c9e1786a201c61814a8
Vaidehee <vaideheet.it18@nsut.ac.in>
2020-06-22 14:22:34 +0530

    feat

--- cricVis/database_ingestion/db_api_test.py
+++ cricVis/database_ingestion/db_api_test.py
@@ -2,7 +2,7 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
-import pprint
+from time import time
 
 cred = credentials.Certificate("firebase-sdk.json")
 
@@ -10,9 +10,24 @@ firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://cricvis-1a59f.firebaseio.com/'
 })
 
-ref = db.reference("/")
-x = ref.child("MatchDescription")
-matref = x.get()
+t1 = time()
+cricVisDB_ref = db.reference("/")
+t2 = time()
+print("Time to get cricVisDB_ref: ", t2-t1)
 
-pp = pprint.PrettyPrinter(indent=2)
-pp.pprint(matref)
+t3 = time()
+match_description_ref = cricVisDB_ref.child("MatchDescription")
+t4 = time()
+print("Time to get match_description_ref: ", t4-t3)
+
+t5 = time()
+match_description_dict = match_description_ref.get()
+t6 = time()
+print("Time to get match_description_dict: ", t6-t5)
+
+"""
+Output (in seconds):
+Time to get cricVisDB_ref:  0.00033593177795410156
+Time to get match_description_ref:  7.3909759521484375e-06
+Time to get match_description_dict:  2.1478004455566406
+"""
