801aa8fdcadfe91a6fa8b42a334d23423f5e9285
Vaidehee <vaideheet.it18@nsut.ac.in>
2020-07-03 03:13:58 +0530

    feat: handle value errors in birth_year field

--- cricVis/database_ingestion/utils/util_fns.py
+++ cricVis/database_ingestion/utils/util_fns.py
@@ -1,16 +1,20 @@
 import pandas as pd
 
 from dateutil.parser import parse
-from dateutil.parser  import _parser
+from dateutil.parser._parser import ParserError
+from datetime import datetime
 
 def extract_date(date_str):
     try:
-        date_str = '{dd}-{mm}-{yyyy}'.format(dd=parse(date_str, fuzzy=True).day,
-                                             mm=parse(date_str, fuzzy=True).month,
-                                             yyyy=parse(date_str, fuzzy=True).year)
+        if(parse(born, fuzzy=True, default=datetime(1,1,1)) != parse(born, fuzzy=True, default=datetime(2,2,2))):
+            raise ValueError
 
-    except(_parser.ParserError):
-        date_str = "unavailable"
+        else:
+            return '{dd}-{mm}-{yyyy}'.format(dd=parse(born, fuzzy=True).day, mm=parse(born, fuzzy=True).month, yyyy=parse(born, fuzzy=True).year)
+
+    except (ValueError, ParserError):
+        return "unavailable"
+
 
 def remove_invalid_chars(df_column):
     df_column = df_column.map(lambda x: x.replace(".", ""))
