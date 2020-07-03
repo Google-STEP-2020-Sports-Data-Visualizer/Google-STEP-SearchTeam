b97fef25c57405e6716f00633eb5a1ef838f1a65
Vaidehee <vaideheet.it18@nsut.ac.in>
2020-07-03 18:42:41 +0530

    chore: refactor

--- cricVis/database_ingestion/utils/dict_to_database.py
+++ cricVis/database_ingestion/utils/dict_to_database.py
@@ -6,6 +6,6 @@ def dict_to_database(schema_dict):
     schema_name = list(schema_dict.keys())[0]
     schema_ref = db_ref.child(schema_name)
 
-    schema_ref.set(schema_dict[schema_name])
+    schema_ref.update(schema_dict[schema_name])
 
     print("Successfully pushed {schema} to database!".format(schema=schema_name))
